ab45ff855b1cc5f519a6bd843c488ff4939edee3
asad <m.asad.s94@gmail.com>
2019-07-23 02:07:48 +0500

    :newspaper: added main structure, install needed packages

---
+++ README.md
@@ -0,0 +1,1 @@
+# Software Engineer Website (React)
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container-fluid body">
+      <div className="row">
+        <p>Software Engineer Website</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
